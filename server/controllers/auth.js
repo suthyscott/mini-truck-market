0dec25135f56f6d1167ea194decd650d0d2fb4af
suthyscott <sutherlandscott21@gmail.com>
2023-05-17 16:03:36 -0600

    foreign keys, secret

---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    register: (req, res) => {
+        console.log('register')
+    },
+    login: (req, res) => {
+        console.log('login')
+    }
+}
--- server/index.js
+++ server/index.js
@@ -12,10 +12,20 @@ const {getAllTrucks} = require('./controllers/trucks.js')
 const {sequelize} = require('./util/database.js')
 const {Users} = require('./models/users.js')
 const {Trucks} = require('./models/trucks.js')
+const {register, login} = require('./controllers/auth.js')
+const {isAuthenticated} = require('./middleware/isAuthenticated.js')
 
-app.get('/api/trucks', getAllTrucks)
+
+Users.hasMany(Trucks)
+Trucks.belongsTo(Users)
+
+app.post('/api/register', register)
+app.post('/api/login', login)
+
+app.get('/api/trucks', isAuthenticated, getAllTrucks)
 
 
+// sequelize.sync({force:true})
 sequelize.sync()
     .then(() => app.listen(SERVER_PORT, () => console.log(`Take us to warp ${SERVER_PORT}!`)))
     .catch(err => console.log(err))
